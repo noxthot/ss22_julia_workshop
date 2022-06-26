b1db842eeecbc7987fa710b98b033796050c7926
noxthot <gregor.ehrensperger@gmx.com>
2022-06-26 15:15:05 +0000

    Deploying to gh-pages from  @ 91f712c516af6d412d0446ce45eb6a16aa8b6bc1 🚀

--- assets/pages/hpc/code/simd_2.jl
+++ assets/pages/hpc/code/simd_2.jl
@@ -1,35 +1,6 @@
 # This file was generated, do not modify it. # hide
-function mysimdsum(a)
-    result = zero(eltype(a))
-    @simd for i in eachindex(a)
-        @inbounds result += a[i]
-    end
-    return result
-end
-
-function mysum2(V)
-    s = zero(eltype(V))
-    for v in V
-        s += v
-    end
-    return s
-end
-
-function mysimdsum2(V)
-    s = zero(eltype(V))
-    @simd for v in V
-        s += v
-    end
-    return s
-end
-
-println("\nSimple mysum(a) = ", mysum(a))
-@btime mysum($a)
-println("\nBuilt in sum(a) = ", sum(a))
-@btime sum($a)
-println("\nSimple mysimdsum = ", mysimdsum(a))
-@btime mysimdsum($a)
-println("\nSimple mysum2 = ", mysum2(a))
-@btime mysum2($a)
-println("\nSimple mysimdsum2 = ", mysimdsum2(a))
-@btime mysimdsum2($a)
+a32 = rand(Float32, length(a)*2)
+println("\nBuilt in sum(FLOAT64) = ", sum(a))
+@btime sum($a32)
+println("\nBuild in sum(FLOAT32) = ", sum(a32))
+@btime sum($a32)
