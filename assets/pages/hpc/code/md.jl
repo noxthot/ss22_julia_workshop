8dc57060ed191ab11f3e0c54d16d3718c20f8b9f
noxthot <gregor.ehrensperger@gmx.com>
2022-06-28 18:26:18 +0000

    Deploying to gh-pages from  @ e249adc28ba1005f62d2df6467147f3b2ed564ae 🚀

--- assets/pages/hpc/code/md.jl
+++ assets/pages/hpc/code/md.jl
@@ -1,2 +1,2 @@
 # This file was generated, do not modify it. # hide
-@code_typed optimize=false mysum([1.0, 2.0, 3.0])
+methods(+)
--- assets/pages/hpc/code/simd_2.jl
+++ assets/pages/hpc/code/simd_2.jl
@@ -1,6 +1,6 @@
 # This file was generated, do not modify it. # hide
-a32 = rand(Float32, length(a)*2)
-println("\nBuilt-in sum(FLOAT64) = ", sum(a))
-@btime sum($a32)
-println("\nBuild-in sum(FLOAT32) = ", sum(a32))
-@btime sum($a32)
+using InteractiveUtils
+
+@code_llvm mysum(a)
+printstyled("\n------Separator-------\n\n"; color = :red)
+@code_llvm mysimdsum(a)
